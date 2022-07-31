bf94e1a1a1fc8b8809415345bedf7c37d17a221e
manishmehta001 <champbabu137@gmail.com>
2022-07-31 22:39:03 +0530

    add to page

--- js/assignment/16julay2assignment.js
+++ js/assignment/16julay2assignment.js
@@ -4,6 +4,11 @@ Requirements:
 "2. Write a function that compares two numbers and returns the larger of the two.
 Bonus: Output appropriate message if numbers cant be compared or are equal"*/
 
+// define the promt
+const ps = require("prompt-sync");
+const prompt = ps();
+var x = prompt('enter first number ');
+var y = prompt('enter second number ');
 function findLarge(x,y)
 {
     if(x>y)
@@ -16,5 +21,4 @@ function findLarge(x,y)
     }
    
 }
-console.log("largest no", findLarge(12,5));
-console.log("largest no", findLarge(3,87));
+console.log("largest no", findLarge(x,y));
