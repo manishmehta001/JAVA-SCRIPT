0bd7a1fe953f53a0062e5a4f7bda61b3403d620d
manishmehta001 <champbabu137@gmail.com>
2022-07-31 16:20:31 +0530

    add to page

--- js/assignment/16julay1Assignment.js
+++ js/assignment/16julay1Assignment.js
@@ -1,3 +1,10 @@
+/*Assignment 1
+
+Requirements:
+"1. Write a function astroguru that:
+A. Takes four arguments: number of children, spouses name, location and job
+B. Output the following string: “You will be a ‘job’ in ‘location’ and married to ‘partner’ with ‘numkids’.”"*/
+
 function astroguru(job,location,spousersname,numkids)
 {
     console.log(
