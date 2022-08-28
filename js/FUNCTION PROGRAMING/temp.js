b85c3e07cb90dba67d21e00ab34b8779fb049ae1
manishmehta001 <champbabu137@gmail.com>
2022-08-29 01:57:46 +0530

    make to changes

--- js/FUNCTION PROGRAMING/temp.js
+++
@@ -1,9 +0,0 @@
-// function ChekForObject(obj){
-//     obj.age=25;
-// }
-// const obj = {name:"x", age:20};
-// // ChekForObject(obj);
-// // console.log(obj);
-// const obj2 =obj;
-// obj2.age =25;
-// console.log(obj);
